05e3e8fb40bfdbc5edc8e9d5242a6dcbb3c31b18
Kjetil Baksaas <kjetil.baksaas@journeyagency.com>
2022-04-26 17:55:47 +0200

    New basebutton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="focus-transition">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "button",
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+button {
+  width: 200px;
+  padding: 20px;
+  margin: 10px 10px;
+  background-color: rgb(233, 162, 32);
+  border: none;
+  font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
