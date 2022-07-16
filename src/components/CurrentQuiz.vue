94b1a73c4267bf2543e8b905113be466d8acb09a
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2022-07-16 21:52:12 +0200

    Rewrite entire component

--- src/components/CurrentQuiz.vue
+++ src/components/CurrentQuiz.vue
@@ -1,51 +1,52 @@
 <template>
   <div v-if="!quizStore.currentQuiz.finished">
-    <h2>Current quiz: {{ quizStore.getCurrentQuiz.name }}</h2>
+    <h2>Current quiz - <i>{{ quizStore.getCurrentQuiz.name }}</i></h2>
     <p>
       Progress {{ quizStore.getCurrentQuiz.currentQuestion }} /
       {{ quizStore.getCurrentQuiz.questions.length }}
     </p>
     <transition name="question-trans" mode="out-in">
-      <question-container
+      <Component :is="questionComponent"
         :question="quizStore.getCurrentQuestion"
-        :key="quizStore.getCurrentQuiz.currentQuestion"
-        @answerQuestion="checkAnswer"
-      />
+        :summaryToggled="false"
+        @update-result="updateResult"
+        :key="quizStore.getCurrentQuestion.number"
+      ></Component>
     </transition>
-    <base-button v-if="showNextQuestion" @click="nextAnswer()" :text="'Next Question'"/>
-    <base-button @click="cancelCurrentQuiz" :text="'Quit quiz'"/>
+    <BaseButton @click="cancelCurrentQuiz" :text="'Quit quiz'" />
+    <BaseButton v-if="showNextQuestion" @click="getNextQuestion()" :text="'Next Question'" />
   </div>
   <div v-else>
-    <quiz-summary />
+    <QuizSummary />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-import { useStore } from "../store/quizStore";
-import QuestionContainer from "@/components/questionContainer.vue";
 import QuizSummary from "@/components/QuizSummary.vue";
+import { defineComponent, ref } from "vue";
+import { useQuizStore } from "../store/quizStore";
+import { useGetQuestionComponent} from "../composables/getQuestionComponent";
+import TextQuestion from "./question-types/TextQuestion.vue";
 
 export default defineComponent({
   components: {
-    QuestionContainer,
     QuizSummary,
+    TextQuestion
   },
   setup() {
-    const quizStore = useStore();
-    const currentQuestion = quizStore.getCurrentQuestion;
+    const quizStore = useQuizStore();
     const showNextQuestion = ref(false);
-    function checkAnswer(alternative: number) {
+    const { getQuestionComponent } = useGetQuestionComponent();
+    const questionComponent = getQuestionComponent(quizStore.getCurrentQuestion.type);
+
+
+    function updateResult(correct: boolean, answeredAlternativeID: number) {
       showNextQuestion.value = true;
-      let correctIndex = currentQuestion.correct;
-      let correct = false;
-      if (alternative === correctIndex) {
-        correct = true;
-        quizStore.updateCorrect(correct);
-      }
+      quizStore.updateResult(correct, answeredAlternativeID);
     }
-    function nextAnswer() {
-      quizStore.nextQuestion();
+
+    function getNextQuestion() {
+      quizStore.setNextQuestion();
       showNextQuestion.value = false;
     }
 
@@ -55,12 +56,13 @@ export default defineComponent({
 
     return {
       quizStore,
+      questionComponent,
       showNextQuestion,
-      checkAnswer,
-      nextAnswer,
-      cancelCurrentQuiz
+      updateResult,
+      getNextQuestion,
+      cancelCurrentQuiz,
     };
-  }
+  },
 });
 </script>
 
@@ -77,5 +79,4 @@ export default defineComponent({
 .question-trans-leave-from {
   opacity: 1;
 }
-
 </style>
--- src/components/QuizSummary.vue
+++ src/components/QuizSummary.vue
@@ -1,27 +1,54 @@
 <template>
-  <h1 class="mb-1">Quiz finished! Your results</h1>
-  <p class="mb-1"> Correct {{ quizStore.currentQuiz.correctAnswered }} of {{ quizStore.currentQuiz.questions.length }} ( {{ percentageCorrect }}% )</p>
-  <base-button @click="restartCurrentQuiz()" :text="'Restart'" />
+  <h1 class="mb-1">Quiz finished!</h1>
+  <p class="mb-1">
+    You managed {{ quizStore.currentQuiz.correctAnswered }} of {{ quizStore.currentQuiz.questions.length }} correct (
+    {{ percentageCorrect }}% )
+  </p>
+
+  <div v-for="question in quizStore.currentQuiz.questions" :key="question.number">
+    <Component
+      :is="getQuestionComponent(question.type)"
+      :question="question"
+      :summaryToggled="true"
+      class="mb-1"
+    />
+  </div>
+  <BaseButton @click="restartCurrentQuiz()" :text="'Restart'" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useStore } from "../store/quizStore";
+import { useGetQuestionComponent } from "../composables/getQuestionComponent";
+import { useQuizStore } from "../store/quizStore";
+import TextQuestion from "./question-types/TextQuestion.vue";
 
 export default defineComponent({
-  components: { },
+  components: {
+    TextQuestion,
+  },
   setup() {
-    const quizStore = useStore();
+    const quizStore = useQuizStore();
+    const currentQuestion = quizStore.getCurrentQuestion;
+    const finishedQuiz = quizStore.currentQuiz;
 
-    const percentageCorrect = +((quizStore.currentQuiz.correctAnswered / quizStore.currentQuiz.questions.length) * 100).toFixed(2);
+    const { getQuestionComponent } = useGetQuestionComponent();
+    const questionComponent = getQuestionComponent(currentQuestion.type);
+
+    const percentageCorrect = +(
+      (quizStore.currentQuiz.correctAnswered / quizStore.currentQuiz.questions.length) *
+      100
+    ).toFixed(2);
 
     function restartCurrentQuiz() {
       quizStore.restartCurrentQuiz();
     }
     return {
-        quizStore,
-        percentageCorrect,
-        restartCurrentQuiz
+      quizStore,
+      finishedQuiz,
+      percentageCorrect,
+      restartCurrentQuiz,
+      getQuestionComponent,
+      questionComponent,
     };
   },
 });
